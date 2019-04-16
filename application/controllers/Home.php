250894b3eb0e69f0af2b1ebc56f22785b14dab5e
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2019-04-16 10:23:50 +0700

    add how it works section

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -8,21 +8,11 @@ class Home extends CI_Controller
     {
         $data['judul'] = "Menjadi jaGO";
         $this->load->view('Templates/header', $data);
-        //$this->load->view('Templates/navbar');
         $this->load->view('Home/s1_hero.php');
         $this->load->view('Home/s1_cariguru.php');
         $this->load->view('Home/s2_fitur.php');
         $this->load->view('Home/s3_tawaran.php');
-        $this->load->view('Templates/footer');
-    }
-
-    public function coba()
-    {
-        $data['judul'] = "Menjadi jaGO";
-        $this->load->view('Templates/header', $data);
-        //$this->load->view('Templates/navbar');
-        $this->load->view('Home/s1_hero.php');
-        $this->load->view('Home/s2_fitur.php');
+        $this->load->view('Home/s4_howitworks.php');
         $this->load->view('Templates/footer');
     }
 }
---
+++ application/views/Home/s4_howitworks.php
@@ -0,0 +1,13 @@
+<section class="py-5">
+	<div class="container">
+		<h4 class="mb-3 text-right txtBlue subJudul"><span>Lihat Baga</span>imana JaGO Bekerja</h4>
+		<div class="my-5 embed-responsive embed-responsive-4by3">
+			<iframe class="embed-responsive-item" src="https://www.youtube.com/watch?v=JyGGLB542ks"
+				allowfullscreen></iframe>
+		</div>
+		<h3 class="txtBlue judul text-center">Bergabung untuk Mulai Mencari Guru</h3>
+		<div class="text-center">
+			<a class="btn bgRed p-3 text-center mt-2 noRadius mbMobile" href="#">Gabung Sekarang</a>
+		</div>
+	</div>
+</section>
