c064de91356c0bce156a260500e1dc7102e95a22
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2019-04-16 09:38:10 +0700

    fix bagian fitur

--- application/views/Home/s2_fitur.php
+++ application/views/Home/s2_fitur.php
@@ -2,22 +2,34 @@
 	<div class="container py-5">
 		<h4 class="mb-3 text-center txtBlue subJudul">Fitur <span>JaGO</span></h4>
 
-		<div class="row mt-5 justify-items-center">
-			<div class="col-sm-6" style="border: red 1px solid">
-				<img class="my-auto float-left" src="<?php echo base_url('webassets/img/icons/location.svg'); ?>" height="70" width="auto">
-				<p class="lead smText" style="border: red 1px solid">Fitur pencarian berdasarkan<br>lokasi terdekat</p>
+		<div class="row mt-5 d-flex align-items-center">
+			<div class="col-sm-6">
+				<div class="fiturCard">
+					<img class="float-left" src="<?php echo base_url('webassets/img/icons/location.svg'); ?>" height="70" width="auto">
+					<h4>Semakin dekat, semakin akurat</h4>
+					<p class="lead smText">Fitur pencarian berdasarkan lokasi terdekat</p>
+				</div>
 			</div>
-			<div class="col-sm-6" style="border: red 1px solid">
-				<img src="<?php echo base_url('webassets/img/icons/scholarship.svg'); ?>" height="70" width="auto" class="float-left">
-				<p class="lead smText my-auto" style="border: red 1px solid">Semua mata pelajaran dengan guru yang berkualitas</p>
+			<div class="col-sm-6">
+				<div class="fiturCard">
+					<img src="<?php echo base_url('webassets/img/icons/scholarship.svg'); ?>" height="70" width="auto" class="float-left">
+					<h4>Banyak pelajaran, makin pintar</h4>
+					<p class="lead smText my-auto">Semua mata pelajaran dengan guru yang berkualitas</p>
+				</div>
 			</div>
-			<div class="col-sm-6" style="border: red 1px solid">
-				<img src="<?php echo base_url('webassets/img/icons/graduates.svg'); ?>" height="70" width="auto" class="float-left">
-				<p class="lead smText">Semua jenjang mulai dari SD, SMP, SMA</p>
+			<div class="col-sm-6">
+				<div class="fiturCard">
+					<img src="<?php echo base_url('webassets/img/icons/graduates.svg'); ?>" height="70" width="auto" class="float-left">
+					<h4>Untuk semua jenjang</h4>
+					<p class="lead smText">Semua jenjang mulai dari SD, SMP, SMA</p>
+				</div>
 			</div>
-			<div class="col-sm-6" style="border: red 1px solid">
-				<img src="<?php echo base_url('webassets/img/icons/trophy.svg'); ?>" height="70" width="auto" class="float-left">
-				<p class="lead smText">Jadi JaGO diberbagai pelajaran</p>
+			<div class="col-sm-6">
+				<div class="fiturCard">
+					<img src="<?php echo base_url('webassets/img/icons/trophy.svg'); ?>" height="70" width="auto" class="float-left">
+					<h4>Raih prestasi gemilang</h4>
+					<p class="lead smText">Jadi JaGO diberbagai pelajaran</p>
+				</div>
 			</div>
 		</div>
 	</div>
