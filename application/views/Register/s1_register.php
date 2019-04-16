5add7aa287272c4a39c2d5a5d667386f4c392e9b
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2019-04-16 14:25:14 +0700

    add some effect

--- application/views/Register/s1_register.php
+++ application/views/Register/s1_register.php
@@ -5,7 +5,7 @@
             </div>
             <div class="col-sm-6 p-5 text-center">
                 <h4 class="txtBlue subJudul">REGISTER</h4>
-                <p class="smText">Bergabunglah Bersama Kami untuk Berbagi Ilmu</p>
+                <p class="smText">Bergabunglah Bersama Kami untuk Saling berbagi</p>
 
                 <div class="container mt-5">
                     <form action="" method="">
@@ -21,7 +21,7 @@
                             <option value="3">Guru</option>
                         </select><br>
                         <button class="btn tombol bgBlue2 mt-5 noRadius" type="submit">Register</button>
-                        <p class="mt-4 text-center tnText lead">Sudah punya akun? <a href="">Klik di sini login</a></p>
+                        <p class="mt-4 text-center tnText lead">Sudah punya akun? <a href="">Klik di sini untuk login</a></p>
                     </form>
                 </div>
             </div>
