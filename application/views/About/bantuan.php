7cfaba8c710168a7177206469f6bc615fa6d2894
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2019-04-23 14:43:12 +0700

    add bantuan page with faq

--- application/views/About/bantuan.php
+++ application/views/About/bantuan.php
@@ -6,7 +6,16 @@
         Contact : 082325069441<br>
         Instagram : @jadijago</p>
 
-        <h4 class="text-center subJudul mb-4 mt-5">FaQ</h4>
-        <p><span style="color: #3A8BBB;">Apa itu JaGO?</p>
+        <h4 class="text-center subJudul mb-4 mt-5">FAQ</h4>
+        <p><span class="mb-3" style="color: #3A8BBB;">Apa itu JaGO?</span><br>
+        JaGO adalah sebuah layanan untuk mencari guru private untuk bimbingan belajar atau les secara online, tapi proses belajar dilakukan di tempat yang sudah Anda tentukan.</p>
+        <p><span class="mb-3" style="color: #3A8BBB;">Siapa saja guru di JaGO?</span><br>
+        Guru di JaGO berasal dari berbagai daerah dan berbagai kalangan dengan mengampu mata pelajaran masing-masing.</p>
+        <p><span class="mb-3" style="color: #3A8BBB;">Bolehkah saya menjadi guru di JaGO?</span><br>
+        Tentu boleh. Namun ada beberapa persyaratan yang harus dipenuhi, salah satunya adalah minimal sedang menempuh S1 dan setingkatnya.</p>
+        <p><span class="mb-3" style="color: #3A8BBB;">Berapa harga layanan di JaGO?</span><br>
+        Harga layanan di JaGO bervariasi tergantung jenjang dan mata pelajaran yang dipilih serta tergantung dari guru karena guru yang memberikan harga sendiri.</p>
+        <p><span class="mb-3" style="color: #3A8BBB;">Bagaimana cara mencari guru di JaGO?</span><br>
+        Silahkan login jika sudah punya akun atau daftar jika belum punya akun. Selanjutnya klik menu cari guru dan cari berdasarkan lokasi, jenjang, atau mata pelajaran. Pilih guru yang dirasa cocok, maka guru akan menghubungi Anda.</p>
 	</div>
 </section>
